856166c6329c5e178cc6c6fbbdbef27218e9a36d
moh-hit <moh.hit1012@gmail.com>
2025-05-17 15:07:02 +0530

    refactor: remove Privacy Policy link from Header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,6 @@
-import React from "react"
 import { IconExternalLink, IconMenu2 } from "@tabler/icons-react"
-import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./sheet"
 import Link from "next/link"
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./sheet"
 
 function Header() {
   return (
@@ -49,15 +48,6 @@ function Header() {
                 <IconExternalLink className="h-4 w-4" />
               </Link>
             </SheetClose>
-            <SheetClose asChild>
-              <Link
-                href="/subxtract/privacy-policy"
-                className="relative after:bg-black after:absolute after:h-0.5 after:rounded-lg after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer text-base md:text-2xl xl:text-lg"
-                target="_blank"
-              >
-                Privacy Policy (Subxtract)
-              </Link>
-            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
@@ -79,13 +69,6 @@ function Header() {
           className="relative after:bg-black after:absolute after:h-0.5 after:rounded-lg after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"
         >
           Blogs
-        </Link>
-        <Link
-          href="/subxtract/privacy-policy"
-          className="relative after:bg-black after:absolute after:h-0.5 after:rounded-lg after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"
-          target="_blank"
-        >
-          Privacy Policy (Subxtract)
         </Link>
         <Link
           href="/resume.pdf"
